30b4fc24b5e8ec8db211aef8df903ae8d85235ea
Javier Prieto <javi@ganso.org>
2025-12-23 00:15:14 +0100

    v1.0

--- res/resources_music.h
+++ res/resources_music.h
@@ -6,6 +6,6 @@ extern const u8 music_geesebumps[1024];
 extern const u8 musica_MerryGentelmen[2304];
 extern const u8 musica_Rudolph[5888];
 extern const u8 musica_SleighRide[42240];
-extern const u8 musica_Fanfarria[268288];
+extern const u8 musica_Fanfarria[268032];
 
 #endif // _RES_RESOURCES_MUSIC_H_
